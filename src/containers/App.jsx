ac080d83d14f914014d61916f92f5fa9dfa45d81
Even Zhao <EvenZhao@Evens-MacBook-Air.local>
2018-12-02 16:09:23 +0800

    dev

---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import { hot } from 'react-hot-loader';
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                ppp
+            </div>
+        )
+    }
+}
+
+export default hot(module)(App);
